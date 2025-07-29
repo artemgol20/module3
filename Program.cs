e74725d535af6fe53e8f88c7ea374c3823cd8dac
artemgol20 <artemgol20@gmail.com>
2025-07-29 20:30:10 +0400

    Добавлены строки для вывода текста в консоль:

--- Program.cs
+++ Program.cs
@@ -2,11 +2,16 @@
 
 class MainClass
 {
-    public static void Main(string[] args)
+    static void Main(string[] args)
     {
-
         const string MyName = "Jane";
+
         Console.WriteLine(MyName);
+
+        Console.WriteLine("Привет, мир");
+        Console.WriteLine("Мне 27 лет");
+        Console.WriteLine($"My name is {MyName}");
+
         Console.ReadKey();
     }
 }
