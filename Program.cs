6e49e4a6a4659cb575b06fd2564754e450fe29da
artemgol20 <artemgol20@gmail.com>
2025-07-29 22:31:25 +0400

    перечисление DaysOfWeek

--- Program.cs
+++ Program.cs
@@ -4,16 +4,18 @@ class MainClass
 {
     public static void Main(string[] args)
     {
+        DaysOfWeek MyFavoriteDay;
 
-        string MyName = "Jane";
-        byte MyAge = 27;
-        bool HaveIApet = true;
-        double MyShoeSize = 37.5;
+        MyFavoriteDay = DaysOfWeek.Friday;
 
-        Console.WriteLine("My name is " + MyName);
-        Console.WriteLine("MyAge " + MyAge);
-        Console.WriteLine("Do I have a pet? " + HaveIApet);
-        Console.WriteLine("My shoe size is " + MyShoeSize);
-
+        Console.WriteLine(MyFavoriteDay);
     }
+}
+
+enum DaysOfWeek : byte
+{
+    Tuesday,
+    Monday,
+    Wednesday,
+    Friday
 }
