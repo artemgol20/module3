23580ae71fd457ab3b3374b40b8e09d0342d94ee
artemgol20 <artemgol20@gmail.com>
2025-07-29 20:40:44 +0400

    Обновлен вывод данных в методе Main
    В методе `Main` класса `MainClass` изменен вывод символа `#` с добавлением перевода строки (`\x23\n`).
    Добавлены новые строки для вывода различных значений:
    - Логические значения `true` и `false`.
    - Целое число `5`.
    - Шестнадцатеричное значение `0x0A`.
    - Двоичное значение `0b11`.
    - Число с плавающей запятой `5.5`.

--- Program.cs
+++ Program.cs
@@ -12,7 +12,14 @@ class MainClass
         Console.WriteLine("\tМне 27 лет");
         Console.WriteLine($"\tMy name is \n {MyName}");
         Console.WriteLine("\u0040");
-        Console.WriteLine("\x23");
+        Console.WriteLine("\x23\n");
+
+        Console.WriteLine(true);
+        Console.WriteLine(false);
+        Console.WriteLine(5);
+        Console.WriteLine(0x0A);
+        Console.WriteLine(0b11);
+        Console.WriteLine(5.5);
         Console.ReadKey();
     }
 }
